1e7cf0919ad251c14733b729bc873a267f548afb
Reitar0 <122444398+Reitar0@users.noreply.github.com>
2023-01-12 00:23:13 +0300

    Кодировка

--- TestWork/TestWork.cpp
+++ TestWork/TestWork.cpp
@@ -1,12 +1,15 @@
 ﻿#include <iostream>
 #include <string>
 #include <ctype.h>
-
+#include <windows.h>
 using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	setlocale(LC_ALL, "Russian");
+
 	string StartWord;
 	cin >> StartWord;
 	string NewWord = StartWord;
